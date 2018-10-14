0f34be37be91e951c062e4ea86515ce4b7f10a9f
Delio <innamorati_2016@libero.it>
2018-10-14 13:28:03 +0100

    newscriptfile

---
+++ testerinofileos.py
@@ -0,0 +1,37 @@
+import os
+from datetime import datetime
+from netmiko import ConnectHandler
+import re
+
+
+ipAddr = input("Enter IP: ")
+usr = input("Enter username: ")
+pwd = input("Enter password: ")
+pwd1 = input("Enter secret: ")
+test = "show version"
+device = ConnectHandler(device_type='cisco_ios', ip=ipAddr, username=usr, password=pwd, secret=pwd1)
+name_of_device = device.find_prompt()
+print(device.find_prompt())  # This is useful to verify our ssh connection by checking the prompt of the device
+realname = re.sub('>', "", name_of_device)
+shrun = device.send_command(test)
+print(shrun)
+filename = ("{0}-{1}.txt".format(datetime.now().strftime("%H-%M-%S-"), name_of_device))
+# with open(os.path.join('/Users/Utente/Backup', filename), "w") as file:
+# file.write(shrun)
+# file.close()
+# Simple write text file function
+
+
+def writeTextFile(_name, _text):
+    #takeme = re.sub('\\', " ", _name)
+    fileName = ("{0}.txt".format(_name))
+    file = open(fileName, 'w')
+    file.write(_text)
+    file.close()
+
+
+try:
+    fileName = ("{0}_{1}".format(realname, datetime.now().strftime('%d-%m-%Y')))
+    writeTextFile(os.path.join('backups', fileName), shrun)  # By not specifying path, it will look into the root directory of where this file is being written from and look into backups folder
+except OSError as err:
+    print("OS Error: {0}".format(err))
